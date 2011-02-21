f03b3d65b41b64514f32257d17609e04701b3eac
Thomas Mansencal <thomas.mansencal@gmail.com>
2011-02-21 15:31:50 +0100

    Code punctuation formatting.

--- utilities/sIBL_GUI_Templates_getTemplatesReleases.py
+++ utilities/sIBL_GUI_Templates_getTemplatesReleases.py
@@ -95,7 +95,7 @@ def getTemplatesReleases():
 		parser = Parser(templates[template])
 		parser.read() and parser.parse()
 
-		LOGGER.info("{0} | '{1}' : '{2}'.".format(getTemplatesReleases.__name__, namespace.getNamespace(template), foundations.parser.getAttributeCompound("Release", parser.getValue("Release", "Template", encode=True)).value))
+		LOGGER.info("{0} | '{1}': '{2}'.".format(getTemplatesReleases.__name__, namespace.getNamespace(template), foundations.parser.getAttributeCompound("Release", parser.getValue("Release", "Template", encode=True)).value))
 
 if __name__ == "__main__":
 	getTemplatesReleases()
