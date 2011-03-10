2b9e7b4e20a96c79536f3dda5bd7dd6ce8d47a54
Thomas Mansencal <thomas.mansencal@gmail.com>
2011-03-10 09:34:14 +0100

    Rename templates and update manual / help files.

--- utilities/sIBL_GUI_Templates_buildTemplatesDocumentation.sh
+++ utilities/sIBL_GUI_Templates_buildTemplatesDocumentation.sh
@@ -7,11 +7,17 @@ export TEMPLATES=../src/templates
 
 alias python=/Library/Frameworks/Python.framework/Versions/2.6/bin/python
 
-#! XSI_MR_Standard Template Documentation Building.
-python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual" "$TEMPLATES/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual.html"  "XSI MR Standard - Template - Manual - Help File"
+#! 3dsMax_MR_Standard Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual.html"  "3dsMax MR Standard - Template - Manual - Help File"
 
-#! Softimage_MR_Standard Template Documentation Building.
-python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual" "$TEMPLATES/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual.html"  "Softimage MR Standard - Template - Manual - Help File"
+#! 3dsMax_Scanline_Standard Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual.html"  "3dsMax Scanline Standard - Template - Manual - Help File"
+
+#! 3dsMax_VRay_Standard Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual.html"  "3dsMax VRay Standard - Template - Manual - Help File"
+
+#! Maya_MR_Lightsmith Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_MR_Lightsmith/help/Maya_MR_Lightsmith Template Manual" "$TEMPLATES/Maya/Maya_MR_Lightsmith/help/Maya_MR_Lightsmith Template Manual.html"  "Maya MR Lightsmith - Template - Manual - Help File"
 
 #! Maya_MR_Standard Template Documentation Building.
 python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_MR_Standard/help/Maya_MR_Standard Template Manual" "$TEMPLATES/Maya/Maya_MR_Standard/help/Maya_MR_Standard Template Manual.html"  "Maya MR Standard - Template - Manual - Help File"
@@ -25,14 +31,17 @@ python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_Turtle_Standard
 #! Maya_VRay_Dome_Light Template Documentation Building.
 python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual" "$TEMPLATES/Maya/Maya_VRay_Dome_Light/help/Maya_VRay_Dome_Light Template Manual.html"  "Maya VRay Dome Light - Template - Manual - Help File"
 
+#! Maya_VRay_Lightsmith Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_VRay_Lightsmith/help/Maya_VRay_Lightsmith Template Manual" "$TEMPLATES/Maya/Maya_VRay_Lightsmith/help/Maya_VRay_Lightsmith Template Manual.html"  "Maya VRay Lightsmith - Template - Manual - Help File"
+
 #! Maya_VRay_Standard Template Documentation Building.
 python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Maya/Maya_VRay_Standard/help/Maya_VRay_Standard Template Manual" "$TEMPLATES/Maya/Maya_VRay_Standard/help/Maya_VRay_Standard Template Manual.html"  "Maya VRay Standard - Template - Manual - Help File"
 
-#! 3dsMax_MR_Standard Template Documentation Building.
-python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_MR_Standard/help/3dsMax_MR_Standard Template Manual.html"  "3dsMax MR Standard - Template - Manual - Help File"
+#! Softimage_MR_Lightsmith Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Softimage/Softimage_MR_Lightsmith/help/Softimage_MR_Lightsmith Template Manual" "$TEMPLATES/Softimage/Softimage_MR_Lightsmith/help/Softimage_MR_Lightsmith Template Manual.html"  "Softimage MR Lightsmith - Template - Manual - Help File"
 
-#! 3dsMax_Scanline_Standard Template Documentation Building.
-python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_Scanline_Standard/help/3dsMax_Scanline_Standard Template Manual.html"  "3dsMax Scanline Standard - Template - Manual - Help File"
+#! Softimage_MR_Standard Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual" "$TEMPLATES/Softimage/Softimage_MR_Standard/help/Softimage_MR_Standard Template Manual.html"  "Softimage MR Standard - Template - Manual - Help File"
 
-#! 3dsMax_VRay_Standard Template Documentation Building.
-python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/3dsMax/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual" "$TEMPLATES/3dsMax/3dsMax_VRay_Standard/help/3dsMax_VRay_Standard Template Manual.html"  "3dsMax VRay Standard - Template - Manual - Help File"
+#! XSI_MR_Standard Template Documentation Building.
+python sIBL_GUI_Templates_textileToHtml.py "$TEMPLATES/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual" "$TEMPLATES/XSI/XSI_MR_Standard/help/XSI_MR_Standard Template Manual.html"  "XSI MR Standard - Template - Manual - Help File"
