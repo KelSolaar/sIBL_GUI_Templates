2dad097af43f83005d9b2601c8ab1720e54fe404
KelSolaar <thomas.mansencal@gmail.com>
2011-07-27 00:09:11 +0100

    Trim lines ending whitespaces.

--- utilities/sIBL_GUI_Templates_textileToHtml.py
+++ utilities/sIBL_GUI_Templates_textileToHtml.py
@@ -73,7 +73,7 @@ core.setVerbosityLevel(3)
 def textileToHtml(fileIn, fileOut, title):
 	"""
 	This Definition Outputs A Textile File To HTML.
-		
+
 	@param fileIn: File To Convert. ( String )
 	@param fileOut: Output File. ( String )
 	@param title: HTML File Title. ( String )
